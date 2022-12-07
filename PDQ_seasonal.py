23720c62f2ecf270f8b6e7078b3678895ec2c711
Olivia Scarborough <olivia.scarborough@generalassemb.ly>
2022-12-07 16:02:12 -0500

    added AIC and BIC tests

--- PDQ_seasonal.py
+++ PDQ_seasonal.py
@@ -15,10 +15,10 @@ ts = df['enrollment count']
 # ts_train = ts[:'2022-06-30']
 # ts_test = ts['2022-06-30':]
 
-result = seasonal_decompose(ts, model='multiplicative', extrapolate_trend='freq')
+result = seasonal_decompose(ts, model='additive', extrapolate_trend='freq')
 
-# result.plot()
-# plt.show()
+result.plot()
+plt.show()
 
 # determine D value. if stationary then 0
 
@@ -51,6 +51,40 @@ while check_stationarity(ts_diff) is False:
 seasonal = result.seasonal
 check_stationarity(seasonal)
 print(d)
+
+# rank AIC and BIC to find best fit
+
+order_aic_bic = []
+
+# Loop over p values from 0-4
+for p in range(5):
+    # Loop over q values from 0-4
+    for q in range(5):
+
+        try:
+            # create and fit ARMA(p,q) model
+            model = sm.tsa.statespace.SARIMAX(seasonal, order=(p, 1, q))
+            results = model.fit()
+
+            # Print order and results
+            order_aic_bic.append((p, q, results.aic, results.bic))
+        except:
+            print(p, q, None, None)
+
+# Make DataFrame of model order and AIC/BIC scores
+order_df = pd.DataFrame(order_aic_bic, columns=['p', 'q', 'aic', 'bic'])
+
+# lets sort them by AIC and BIC
+
+# Sort by AIC
+print("Models sorted by AIC ")
+print("\n")
+print(order_df.sort_values('aic').reset_index(drop=True))
+
+# Sort by BIC
+print("Models sorted by BIC ")
+print("\n")
+print(order_df.sort_values('bic').reset_index(drop=True))
 
 # determine P value. count the number of lags significantly out of the limit. This can vary based on interpretation
 
